effcb183eb9928209cdb8fbe1ba753d4126fc13c
Chaney <chaneyfonseca@gmail.com>
2024-10-27 14:09:21 -0500

    use azure

--- providers.tf
+++ providers.tf
@@ -1,14 +1,12 @@
 terraform {
   required_providers {
-    aws = {
-      source = "hashicorp/aws"
-      version = "5.31.0"
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=4.4.0"
     }
   }
 }
 
-provider "aws" {
-  region = "us-east-1"
-  profile = "Helen"
-  # Configuration options
+provider "azurerm" {
+  features {}
 }
